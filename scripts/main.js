172a030487abb9a7b98626e917f3ac2f77d270c0
kimbalear <kimbalear@gmail.com>
2020-11-26 13:30:36 +0100

    solved height lines

--- scripts/main.js
+++ scripts/main.js
@@ -1,147 +1,95 @@
 jQuery(document).ready(function () {
-    var S1_c1 = $(".S1_c1").height();
-    $(".S1_c2").height(S1_c1);
 
-    var S2_c1 = $(".S2_c1").height();
-    $(".S2_c2").height(S2_c1);
+    //TAB-1 | Summary
 
-
-    var S3_c1 = $(".S3_c1").height();
-    $(".S3_c2").height(S3_c1);
-
-    /******/
-    for(i=7; i <=12; i++){
-        var vhi = 'S'+i+'_c1';
-        v2 = 'S'+i+'_c2';
-        var vhi = $(".S7_c1").height();
-        $(".S7_c2").height(vhi);
+    for (i = 1; i <= 12; i++) {
+        var hight_Sc1 = '.S' + i + '_c1';
+        var hight_Sc2 = '.S' + i + '_c2';
+        var vS_hight = $(hight_Sc1).height();
+        $(hight_Sc2).height(vS_hight);
     }
 
-    var S7_c1 = $(".S7_c1").height();
-    $(".S7_c2").height(S7_c1);
+    //TAB-2 | O1
 
-    var S8_c1 = $(".S8_c1").height();
-    $(".S8_c2").height(S8_c1);
+    for (i = 11; i <= 12; i++) {
+        var hight_O1c1 = '.O1' + i + '_c1';
+        var hight_O1c2 = '.O1' + i + '_c2';
+        var vO1_hight = $(hight_O1c1).height();
+        $(hight_O1c2).height(vO1_hight);
+    }
 
-    var S9_c1 = $(".S9_c1").height();
-    $(".S9_c2").height(S9_c1);
+    //TAB-3 | O2
 
-    var S10_c1 = $(".S10_c1").height();
-    $(".S10_c2").height(S10_c1);
+    for (i = 8; i <= 31; i++) {
+        var hight_O2c1 = '.O2' + i + '_c1';
+        var hight_O2c2 = '.O2' + i + '_c2';
+        var vO2_hight = $(hight_O2c1).height();
+        $(hight_O2c2).height(vO2_hight);
+    }
 
-    var S11_c1 = $(".S11_c1").height();
-    $(".S11_c2").height(S11_c1);
+    //TAB-4 | O3
 
-    var S12_c1 = $(".S12_c1").height();
-    $(".S12_c2").height(S12_c1);
+    for (i = 1; i <= 29; i++) {
+        var hight_O3c1 = '.O3' + i + '_c1';
+        var hight_O3c2 = '.O3' + i + '_c2';
+        var vO3_hight = $(hight_O3c1).height();
+        $(hight_O3c2).height(vO3_hight);
+    }
 
-    /******/
+    //TAB-5 | O4
 
-    var O11_c1 = $(".O11_c1").height();
-    $(".O11_c2").height(O11_c1);
+    for (i = 1; i <= 14; i++) {
+        var hight_O4c1 = '.O4' + i + '_c1';
+        var hight_O4c2 = '.O4' + i + '_c2';
+        var vO4_hight = $(hight_O4c1).height();
+        $(hight_O4c2).height(vO4_hight);
+    }
 
-    /******/
+    //TAB-6 | O5
 
-    var O28_c1 = $(".O28_c1").height();
-    $(".O28_c2").height(O28_c1);
+    for (i = 1; i <= 54; i++) {
+        var hight_O5c1 = '.O5' + i + '_c1';
+        var hight_O5c2 = '.O5' + i + '_c2';
+        var vO5_hight = $(hight_O5c1).height();
+        $(hight_O5c2).height(vO5_hight);
+    }
 
-    var O29_c1 = $(".O29_c1").height();
-    $(".O29_c2").height(O29_c1);
+    //TAB-7 | O6
+    for (i = 1; i <= 27; i++) {
+        var hight_O6c1 = '.O6' + i + '_c1';
+        var hight_O6c2 = '.O6' + i + '_c2';
+        var vO6_hight = $(hight_O6c1).height();
+        $(hight_O6c2).height(vO6_hight);
+    }
 
-    var O210_c1 = $(".O210_c1").height();
-    $(".O210_c2").height(O210_c1);
+    //TAB-8 | Business
+    for (i = 1; i <= 11; i++) {
+        var hight_BSSc1 = '.BSS' + i + '_c1';
+        var hight_BSSc2 = '.BSS' + i + '_c2';
+        var vBSS_hight = $(hight_BSSc1).height();
+        $(hight_BSSc2).height(vBSS_hight);
+    }
 
-    var O211_c1 = $(".O211_c1").height();
-    $(".O211_c2").height(O211_c1);
+    //TAB-9 | Humanitarian Strategy Milestones
+    for (i = 1; i <= 19; i++) {
+        var hight_HSMc1 = '.HSM' + i + '_c1';
+        var hight_HSMc2 = '.HSM' + i + '_c2';
+        var vHSM_hight = $(hight_HSMc1).height();
+        $(hight_HSMc2).height(vHSM_hight);
+    }
 
-    var O212_c1 = $(".O212_c1").height();
-    $(".O212_c2").height(O212_c1);
+    //TAB-10 | Advocacy Common Agenda
+    for (i = 1; i <= 4; i++) {
+        var hight_ACAc1 = '.ACA' + i + '_c1';
+        var hight_ACAc2 = '.ACA' + i + '_c2';
+        var vACA_hight = $(hight_ACAc1).height();
+        $(hight_ACAc2).height(vACA_hight);
 
-    var O213_c1 = $(".O213_c1").height();
-    $(".O213_c2").height(O213_c1);
-
-    var O214_c1 = $(".O214_c1").height();
-    $(".O214_c2").height(O214_c1);
-
-    var O215_c1 = $(".O215_c1").height();
-    $(".O215_c2").height(O215_c1);
-
-    var O216_c1 = $(".O216_c1").height();
-    $(".O216_c2").height(O216_c1);
-
-    var O210_c1 = $(".O210_c1").height();
-    $(".O210_c2").height(O210_c1);
-
-    var O210_c1 = $(".O210_c1").height();
-    $(".O210_c2").height(O210_c1);
-
-    var O210_c1 = $(".O210_c1").height();
-    $(".O210_c2").height(O210_c1);
-
-    var O210_c1 = $(".O210_c1").height();
-    $(".O210_c2").height(O210_c1);
-
-    var O210_c1 = $(".O210_c1").height();
-    $(".O210_c2").height(O210_c1);
-
-    var O210_c1 = $(".O210_c1").height();
-    $(".O210_c2").height(O210_c1);
-
-    var O210_c1 = $(".O210_c1").height();
-    $(".O210_c2").height(O210_c1);
-    
-
-    
-
-
-
+        //$(hight_ACAc1).css('background-color', 'red');
+        //$(hight_ACAc2).css('background-color', 'red');
+    }
 
 
-
-    var alt_s31 = $(".h_st_s31").height();
-    $(".h_l_s31").height(alt_s31);
-
-    var alt_s32 = $(".h_st_s32").height();
-    $(".h_l_s32").height(alt_s32);
-
-    var alt_s310 = $(".h_st_s310").height();
-    $(".h_l_s310").height(alt_s310);
-
-    var alt_s311 = $(".h_st_s311").height();
-    $(".h_l_s311").height(alt_s311);
-
-    var alt_s327 = $(".h_st_s327").height();
-    $(".h_l_s327").height(alt_s327);
-
-    var alt_s328 = $(".h_st_s328").height();
-    $(".h_l_s328").height(alt_s328);
-
-    var alt_s329 = $(".h_st_s329").height();
-    $(".h_l_s329").height(alt_s329);
-
-    var alt_s430 = $(".h_st_s430").height();
-    $(".h_l_s430").height(alt_s430);
-
-    var alt_s514 = $(".h_st_s514").height();
-    $(".h_l_s514").height(alt_s514);
-
-    var alt_s524 = $(".h_st_s524").height();
-    $(".h_l_s524").height(alt_s524);
-
-    var alt_s525 = $(".h_st_s525").height();
-    $(".h_l_s525").height(alt_s525);
-
-    var alt_s526 = $(".h_st_s526").height();
-    $(".h_l_s526").height(alt_s526);
-
-    var alt_s527 = $(".h_st_s527").height();
-    $(".h_l_s527").height(alt_s527);
-
-    var alt_s528 = $(".h_st_s528").height();
-    $(".h_l_s528").height(alt_s528);
-
-    var alt_s65 = $(".h_st_s65").height();
-        $(".h_l_s65").height(alt_s65);
 
     $("#selectbox1").change(function () {
         var str = "";
@@ -178,67 +126,67 @@ jQuery(document).ready(function () {
 
     $(window).resize(function () {
         var S1_c1 = $(".S1_c1").height();
-    $(".S1_c2").height(S1_c1);
+        $(".S1_c2").height(S1_c1);
 
-    var S2_c1 = $(".S2_c1").height();
-    $(".S2_c2").height(S2_c1);
+        var S2_c1 = $(".S2_c1").height();
+        $(".S2_c2").height(S2_c1);
 
 
-    var S3_c1 = $(".S3_c1").height();
-    $(".S3_c2").height(S3_c1);
+        var S3_c1 = $(".S3_c1").height();
+        $(".S3_c2").height(S3_c1);
 
-    var S3_c1 = $(".S3_c1").height();
-    $(".S3_c2").height(S3_c1);
+        var S3_c1 = $(".S3_c1").height();
+        $(".S3_c2").height(S3_c1);
 
-    var alt_s8 = $(".h_st_s8").height();
-    $(".h_l_s8").height(alt_s8);
+        var alt_s8 = $(".h_st_s8").height();
+        $(".h_l_s8").height(alt_s8);
 
-    var alt_s11 = $(".h_st_s11").height();
-    $(".h_l_s11").height(alt_s11);
+        var alt_s11 = $(".h_st_s11").height();
+        $(".h_l_s11").height(alt_s11);
 
-    var alt_s31 = $(".h_st_s31").height();
-    $(".h_l_s31").height(alt_s31);
+        var alt_s31 = $(".h_st_s31").height();
+        $(".h_l_s31").height(alt_s31);
 
-    var alt_s32 = $(".h_st_s32").height();
-    $(".h_l_s32").height(alt_s32);
+        var alt_s32 = $(".h_st_s32").height();
+        $(".h_l_s32").height(alt_s32);
 
-    var alt_s310 = $(".h_st_s310").height();
-    $(".h_l_s310").height(alt_s310);
+        var alt_s310 = $(".h_st_s310").height();
+        $(".h_l_s310").height(alt_s310);
 
-    var alt_s311 = $(".h_st_s311").height();
-    $(".h_l_s311").height(alt_s311);
+        var alt_s311 = $(".h_st_s311").height();
+        $(".h_l_s311").height(alt_s311);
 
-    var alt_s327 = $(".h_st_s327").height();
-    $(".h_l_s327").height(alt_s327);
+        var alt_s327 = $(".h_st_s327").height();
+        $(".h_l_s327").height(alt_s327);
 
-    var alt_s328 = $(".h_st_s328").height();
-    $(".h_l_s328").height(alt_s328);
+        var alt_s328 = $(".h_st_s328").height();
+        $(".h_l_s328").height(alt_s328);
 
-    var alt_s329 = $(".h_st_s329").height();
-    $(".h_l_s329").height(alt_s329);
+        var alt_s329 = $(".h_st_s329").height();
+        $(".h_l_s329").height(alt_s329);
 
-    var alt_s430 = $(".h_st_s430").height();
-    $(".h_l_s430").height(alt_s430);
+        var alt_s430 = $(".h_st_s430").height();
+        $(".h_l_s430").height(alt_s430);
 
-    var alt_s514 = $(".h_st_s514").height();
-    $(".h_l_s514").height(alt_s514);
+        var alt_s514 = $(".h_st_s514").height();
+        $(".h_l_s514").height(alt_s514);
 
-    var alt_s524 = $(".h_st_s524").height();
-    $(".h_l_s524").height(alt_s524);
+        var alt_s524 = $(".h_st_s524").height();
+        $(".h_l_s524").height(alt_s524);
 
-    var alt_s525 = $(".h_st_s525").height();
-    $(".h_l_s525").height(alt_s525);
+        var alt_s525 = $(".h_st_s525").height();
+        $(".h_l_s525").height(alt_s525);
 
-    var alt_s526 = $(".h_st_s526").height();
-    $(".h_l_s526").height(alt_s526);
+        var alt_s526 = $(".h_st_s526").height();
+        $(".h_l_s526").height(alt_s526);
 
-    var alt_s527 = $(".h_st_s527").height();
-    $(".h_l_s527").height(alt_s527);
+        var alt_s527 = $(".h_st_s527").height();
+        $(".h_l_s527").height(alt_s527);
 
-    var alt_s528 = $(".h_st_s528").height();
-    $(".h_l_s528").height(alt_s528);
+        var alt_s528 = $(".h_st_s528").height();
+        $(".h_l_s528").height(alt_s528);
 
-    var alt_s65 = $(".h_st_s65").height();
+        var alt_s65 = $(".h_st_s65").height();
         $(".h_l_s65").height(alt_s65);
 
     });
